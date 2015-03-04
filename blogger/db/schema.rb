8ea521b354c758e6e2f9c77372eb81fc25436062
Mike Lamb <michaellamb@users.noreply.github.com>
2015-03-04 13:17:33 -0500

    added a few gems

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302040840) do
+ActiveRecord::Schema.define(version: 20150303062938) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20150302040840) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "attachments", ["article_id"], name: "index_attachments_on_article_id"
 
   create_table "comments", force: :cascade do |t|
     t.string   "author_name"
